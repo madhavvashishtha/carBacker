0c6c30f08ef0bbaf5b8ad726970f539861133e5b
madhavvashishtha <madhav314159@gmail.com>
2024-05-08 07:10:50 +0530

    pp#4

--- index.js
+++ index.js
@@ -37,18 +37,7 @@ async function run() {
   }
 }
 
-async function run2() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db(dbName).command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!"+uri+':'+sec);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
+
 
 
 app.get('/api/createUser',(req, res) => {
