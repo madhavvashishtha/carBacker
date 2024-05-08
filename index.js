cad932b48520124f8ea79aa9bb8c1d35365a1f24
madhavvashishtha <madhav314159@gmail.com>
2024-05-08 07:10:00 +0530

    pp#5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const {mongoUrl} = require('./.env')
+//const {mongoUrl} = require('./.env') 
 const express = require('express');
 const app = express();
+const sec='23'
 
 //console.log(`Your port is ${process.env.PORT}`); // undefined
 //const dotenv = require('dotenv');
@@ -29,7 +30,20 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db(dbName).command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!"+uri);
+    console.log("Pinged your deployment. You successfully connected to MongoDB!"+uri+':'+sec);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function run2() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!"+uri+':'+sec);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
