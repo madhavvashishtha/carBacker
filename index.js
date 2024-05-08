70bdce1e45f283a7ff2ea248533d114a0321a6e2
madhavvashishtha <madhav314159@gmail.com>
2024-05-08 06:03:55 +0530

    push createrFun

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://madhav314159:rcilybQqqZyRXpt1@cluster0.nw8qfuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const{mongoUrl,PORT} = require('./configs/.env')
+const express = require('express');
+const app = express();
 
+const dbName = 'Cluster0';
+
+
+const uri = mongoUrl;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,4 +28,54 @@ async function run() {
     await client.close();
   }
 }
-run().catch(console.dir);
+
+
+app.get('/api/createUser',(req, res) => {
+  if (req.url === '/createUser' && req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+          body += chunk.toString();
+      });
+      req.on('end', () => {
+          const userData = JSON.parse(body);
+          mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+              if (err) {
+                  res.writeHead(500, { 'Content-Type': 'application/json' });
+                  res.end(JSON.stringify({ message: 'Internal Server Error' }));
+                  return;
+              }
+              const db = client.db(dbName);
+              const users = db.collection('users');
+              bcrypt.hash(userData.password, 10, (err, hash) => {
+                  if (err) {
+                      res.writeHead(500, { 'Content-Type': 'application/json' });
+                      res.end(JSON.stringify({ message: 'Internal Server Error' }));
+                      client.close();
+                      return;
+                  }
+                  const user = { username: userData.username, password: hash };
+                  users.insertOne(user, (err, result) => {
+                      if (err) {
+                          res.writeHead(500, { 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify({ message: 'Internal Server Error' }));
+                      } else {
+                          const token = jwt.sign({ username: user.username }, 'secretKey', { expiresIn: '1h' });
+                          res.writeHead(200, { 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify({ message: 'User created successfully', token: token }));
+                      }
+                      client.close();
+                  });
+              });
+          });
+      });
+  } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Not Found' }));
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+//run().catch(console.dir);
