778d31e48595d66fbbfd0a0a7edbedc7a01c2bc3
madhavvashishtha <madhav314159@gmail.com>
2024-05-13 07:08:52 +0530

    stillFed

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const promise2PushInMain = new Promise(async (resolve, reject) => {
 });
 
 
-await new Promise.all([promise1PushInProfi, promise2PushInMain])
+ Promise.all([promise1PushInProfi, promise2PushInMain])
   .then((results) => results.forEach((result) => {
      // console.log(result)
    res.setHeader('Content-Type', 'text/plain');
